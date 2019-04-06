ca87fe6bd4b3214cc732f9d02679a44c1c9773f9
Brendan John <bjohn454@gmail.com>
2019-04-05 23:27:21 -0400

    added tech/contact

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,6 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  techArr = [
+    {
+      name: 'OSISoft PI',
+      imgSrc: './assets/img/osipi.jpeg'
+    },
+    {
+      name: 'IBM Maximo',
+      imgSrc: './assets/img/ibm-maximo.png'
+    },
+    {
+      name: 'Angular',
+      imgSrc: './assets/img/angular.svg'
+    },
+    {
+      name: 'Docker',
+      imgSrc: './assets/img/docker.svg'
+    },
+    {
+      name: 'Nginx',
+      imgSrc: './assets/img/Nginx_logo.svg'
+    },
+    {
+      name: 'Python',
+      imgSrc: './assets/img/Python.svg'
+    },
+    {
+      name: 'Mongo',
+      imgSrc: './assets/img/mongodb.svg'
+    },
+    {
+      name: 'Golang',
+      imgSrc: './assets/img/Go_Logo.svg'
+    },
+  ]
   constructor() { }
 
   ngOnInit() {
