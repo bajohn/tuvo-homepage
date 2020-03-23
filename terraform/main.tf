a760ac1c9ec38184123e4f12cad0f471593a41cc
Brendan John <bjohn454@gmail.com>
2020-03-22 22:41:40 -0400

    cloudfront main.tf matches deployed state

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,10 +6,10 @@ provider "aws" {
 
 resource "aws_cloudfront_distribution" "tuvo_distribution" {
   origin {
+    domain_name = "tuvo.s3.amazonaws.com"
     origin_id   = "S3-tuvo/tuvo.s3.amazonaws.com/index/dist/index.html"
-    domain_name = "d2w67oumh7fi0w.cloudfront.net" // aws_cloudfront_distribution.tuvo_distribution.domain_name
   }
-
+  aliases = ["tuvo.io"]
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
@@ -26,13 +26,29 @@ resource "aws_cloudfront_distribution" "tuvo_distribution" {
 
     viewer_protocol_policy = "allow-all"
     min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    default_ttl            = 86400
+    max_ttl                = 31536000
   }
   enabled         = true
   is_ipv6_enabled = true
   viewer_certificate {
-    acm_certificate_arn = "arn:aws:acm:us-east-1:748004005034:certificate/6f3a889d-9af4-40fd-bdb4-dc65030d4951"
+    acm_certificate_arn      = "arn:aws:acm:us-east-1:748004005034:certificate/6f3a889d-9af4-40fd-bdb4-dc65030d4951"
+    minimum_protocol_version = "TLSv1.1_2016"
+    ssl_support_method       = "sni-only"
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
   }
   restrictions {
     geo_restriction {
